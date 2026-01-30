94806dc7116765f39b0cb02aa52df1e5edf72e9c
Vanshika Goel <vanshika.gitrepo@gmail.com>
2026-01-30 20:04:05 +0530

    Inception Episode 2

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "i am a H1 Tag"),
+    React.createElement("h2", { id: "heading2" }, "i am a H2 Tag"),
+  ]),
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
